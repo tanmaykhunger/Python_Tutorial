505a8c9931f44f1ac09dc094e45d98ce598a2788
Tanmay Khunger <117931543+tanmaykhunger@users.noreply.github.com>
2022-12-29 20:09:12 +0530

    more new changes

--- Type_Conversion.py
+++ Type_Conversion.py
@@ -1,5 +1,8 @@
+import datetime
 birth_year = input("Birth year: ")
+today = datetime.date.today()
+year = today.year
 print(type(birth_year))
-age = 2022 - int(birth_year)
+age = year - int(birth_year)
 print(type(age))
 print(f'You are {age} years old.')
