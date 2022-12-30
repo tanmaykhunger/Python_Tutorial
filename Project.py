225b643eb391d94699d7f335c104d2d37f5c224a
Tanmay Khunger <117931543+tanmaykhunger@users.noreply.github.com>
2022-12-30 20:01:01 +0530

    new Changes a lot

--- Project.py
+++
@@ -1,6 +0,0 @@
-import datetime
-birthday = datetime.date(2014,2,21)
-now = datetime.date.today()
-delta = now - birthday
-print(delta.days)
-
---
+++ Strings.py
@@ -0,0 +1,17 @@
+course = 'Python\'s for Beginners'
+course1 = 'Python for "Beginners"'
+          #0123456789123456789!@
+print(course)
+print(course1)
+print(course[:])
+message = '''
+Hi Chahat,
+Here is our first email to you.
+
+Thank you,
+Learn with Tanmay
+'''
+print(message)
+
+
+
