54dbb04bb160fbfbc46778109efa6bc1b7da431f
Tanmay Khunger <117931543+tanmaykhunger@users.noreply.github.com>
2023-01-29 11:27:04 +0530

    New changer

---
+++ Keyword_Arguments.py
@@ -0,0 +1,9 @@
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}!")
+    print("Welcome aboard")
+
+
+print("Start")
+greet_user(first_name="Tanmay", last_name="Khunger")
+greet_user(first_name="Chahat", last_name="Khunger")
+print("Finish")
---
+++ Parameters.py
@@ -0,0 +1,9 @@
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}!")
+    print("Welcome aboard")
+
+
+print("Start")
+greet_user("Tanmay", "Khunger")
+greet_user("Chahat", "Khunger")
+print("Finish")
